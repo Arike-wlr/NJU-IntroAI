2404010622104534a22d5fc016c1cae5fb3cd2b9
Arike <1637928975@qq.com>
2025-09-04 13:32:42 +0800

    ver 2.0 of task 1 and allow 'mode' to be passed as a parameter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import copy
-
+import  argparse
 from env import BaitEnv
-
+import logging
+import sys
 from controllers.random import RandomAgent
 from controllers.depthfirst import DFSAgent
 from controllers.limitdepthfirst import LimitedDFSAgent
@@ -15,29 +16,44 @@ if __name__ == "__main__":
     env = BaitEnv(level=level, render=False)
     
     # actions: 0 noop, 1 left, 2 right, 3 down, 4 up
-    
-    mode = "random" # "play", "random", "depthfirst", "limitdepthfirst", "Astar", "MCTS"
+    parser = argparse.ArgumentParser(description="Bait 游戏，请选择执行模式")
+    parser.add_argument(
+        "--mode",
+        choices=["random" , "play", "depthfirst", "limitdepthfirst", "Astar", "MCTS"],
+        required=True,
+        help="运行模式:random--随机运行；depthfirst--深度优先搜索；。。。"
+    )
+
+    args = parser.parse_args()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)]
+    )
+
     action_lst = None
-    if mode == "play":
+    if args.mode == "play":
         # input your own actions here
         tick_max = 30
         action_lst = [3, 2, 3, 1, 3, 4, 4, 4, 1, 0]
-    elif mode == "random":
+    elif args.mode == "random":
         tick_max = 30
         agent = RandomAgent(env, tick_max)
-    elif mode == "depthfirst":
+    elif args.mode == "depthfirst":
         tick_max = 10000
         agent = DFSAgent(env, tick_max)
         action_lst = agent.solve()
-    elif mode == "limitdepthfirst":
+    elif args.mode == "limitdepthfirst":
         tick_max = 100
         agent = LimitedDFSAgent(env, tick_max)
-    elif mode == "Astar":
+    elif args.mode == "Astar":
         tick_max = 100
         agent = AstarAgent(env, tick_max)
-    elif mode == "MCTS":
+    elif args.mode == "MCTS":
         tick_max = 1000
         agent = MCTSAgent(env, tick_max)
+    else:
+        raise ValueError(f"未知模式: {args.mode}")
 
     print("Action list:", action_lst)
     action_lst_len = len(action_lst) if action_lst else 1e8
